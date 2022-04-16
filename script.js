7192e6f2708eb61f96d06d2f6e909c213b063fe3
victor bobkov <victorbobkov@gmail.com>
2022-04-17 03:14:11 +0400

    Store data in localstorage

--- script.js
+++ script.js
@@ -34,12 +34,25 @@ const lightMode = () => {
 const switchTheme = (event) => {
    if (event.target.checked) {
       document.documentElement.setAttribute('data-theme', 'dark')
+      localStorage.setItem('theme', 'dark')
       darkMode()
    } else {
       document.documentElement.setAttribute('data-theme', 'light')
+      localStorage.setItem('theme', 'light')
       lightMode()
    }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme)
+
+// Check Local Storage for theme
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme) {
+   document.documentElement.setAttribute('data-theme', currentTheme)
+
+   if (currentTheme === 'dark') {
+      toggleSwitch.checked = true
+      darkMode()
+   }
+}
